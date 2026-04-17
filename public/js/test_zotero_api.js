9f722a8b9edd07f1c7ffb7b88ba2dbd5acaf9a7e
Adam Sapp <30439118+asapp25@users.noreply.github.com>
2026-04-17 12:23:09 -0400

    Update test_zotero_api.js

--- public/js/test_zotero_api.js
+++ public/js/test_zotero_api.js
@@ -1,10 +1,11 @@
 
 const { default: api } = ZoteroApiClient;
 
-console.log(api);
+//console.log(api);
+const myapi = api().library('group', 6379466);
 
-const response = await api().library('group', 6379466).items().get();
-console.log(response)
+const response = await myapi.items().get();
+//console.log(response)
 
 const items = response.raw;
 
