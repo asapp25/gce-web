f133725abfa43a2bb5dc9e1a9a5f3a4d51ac76fb
Adam Sapp <30439118+asapp25@users.noreply.github.com>
2026-04-17 11:48:27 -0400

    Update test_zotero_api.js

--- public/js/test_zotero_api.js
+++ public/js/test_zotero_api.js
@@ -2,6 +2,10 @@ const { default: api } = ZoteroApiClient;
 
 const response = api().library('group', 6379466).items().get();
 console.log(response)
+
+const resp = response.getResponseType();
+console.log(resp);
+
 const items = response.getLinks();
 
 
