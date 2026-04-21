0af34740e5fa93ff43e4fc8b41a438febe0c58d2
Adam Sapp <30439118+asapp25@users.noreply.github.com>
2026-04-21 10:03:39 -0400

    bios

--- public/js/personnel_page.js
+++ public/js/personnel_page.js
@@ -1,7 +1,7 @@
 let jsonData;
 
 window.addEventListener("load", () => {
-    fetch("./json/Personnel.json")
+    fetch("./json/personnelBio.json")
     .then((res) => res.json())
     .then((data) => (jsonData = data))
     .catch((err) => console.log(err));
@@ -9,5 +9,15 @@ window.addEventListener("load", () => {
 
 const paramsString = window.location.search;
 const searchParams = new URLSearchParams(paramsString);
+
+const id = searchParams.get("id");
+
+const user = jsonData.find(item = item.NameID == id);
+
+const nametxt = user.FriendlyName;
+const email = user.Email;
+const roleName = user.RoleName;
+
+const name_div = document.getElementById('name').appendChild(nametxt);
 
 console.log(searchParams.get("id"));
